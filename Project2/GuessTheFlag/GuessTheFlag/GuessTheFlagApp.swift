66a4bcdd57991430cd243f0edbac99c6b68a6801
Michael Page <mobilepage@me.com>
2022-08-11 15:37:42 +0930

    Add new GuessTheFlag Xcode project

---
+++ Project2/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Michael Page on 11/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
