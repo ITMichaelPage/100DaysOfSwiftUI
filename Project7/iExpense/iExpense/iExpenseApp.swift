513c155e55dc03f5f5a12ce015b1a3bcfde04b2e
Michael Page <mobilepage@me.com>
2022-08-21 16:11:18 +0930

    iExpense tutorial provided code

---
+++ Project7/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Michael Page on 19/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
