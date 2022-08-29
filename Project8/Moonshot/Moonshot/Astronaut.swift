bd62d3996c0e8c430667bd832ff0895e70de1aad
Michael Page <mobilepage@me.com>
2022-08-29 21:14:25 +0930

    Moonshot tutorial provided code

---
+++ Project8/Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Michael Page on 21/8/2022.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
